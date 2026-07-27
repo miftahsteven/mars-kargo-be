b4fea206bcb130e251b73d911b0a40c88b598480
Miftah Syarief <miftahsyarief@mscode-lab.local>
2026-07-27 10:41:54 +0700

    ignore dist

--- dist/routes/notificationRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const notificationController_js_1 = require("../controllers/notificationController.js");
-const router = (0, express_1.Router)();
-router.get('/user/:userId', notificationController_js_1.NotificationController.getNotifications);
-router.patch('/:id/read', notificationController_js_1.NotificationController.markAsRead);
-exports.default = router;
